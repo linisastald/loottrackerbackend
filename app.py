88efd0f734deb1a17ad8b35cd6914a3c0791a9b6
Daniel <git@danielkempson.com>
2023-05-20 01:45:57 -0400

    asd

--- app.py
+++ app.py
@@ -9,12 +9,44 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 import models
+from flask import jsonify
 
 
-@app.route('/campaign', methods=['GET'])
-def get_campaigns():
-    campaigns = models.Campaign.query.all()
-    return {"campaigns": [campaign.serialize() for campaign in campaigns]}
+@app.route('/appraisal', methods=['GET'])
+def get_appraisals():
+    appraisals = models.Appraisal.query.all()
+    return jsonify({"appraisals": [appraisal.serialize() for appraisal in appraisals]})
+
+
+@app.route('/campaign_character', methods=['GET'])
+def get_campaign_characters():
+    campaign_characters = models.CampaignCharacter.query.all()
+    return jsonify(
+        {"campaign_characters": [campaign_character.serialize() for campaign_character in campaign_characters]})
+
+
+@app.route('/character', methods=['GET'])
+def get_characters():
+    characters = models.Character.query.all()
+    return jsonify({"characters": [character.serialize() for character in characters]})
+
+
+@app.route('/gold', methods=['GET'])
+def get_golds():
+    golds = models.Gold.query.all()
+    return jsonify({"golds": [gold.serialize() for gold in golds]})
+
+
+@app.route('/item', methods=['GET'])
+def get_items():
+    items = models.Item.query.all()
+    return jsonify({"items": [item.serialize() for item in items]})
+
+
+@app.route('/itemref', methods=['GET'])
+def get_itemrefs():
+    itemrefs = models.Itemref.query.all()
+    return jsonify({"itemrefs": [itemref.serialize() for itemref in itemrefs]})
 
 
 if __name__ == '__main__':
